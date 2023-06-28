5ed7c50a75304395cb928748308aee24c5c9fdae
Andrii-Val <andrii.val1006@gmail.com>
2023-06-28 13:21:39 +0300

    78

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
 
+
+
+
+const saveCurrentTime =throttle(() =>{
+    player.getCurrentTime().then(currentTime =>{
+        localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime))
+
+    } );
+}, 1000)
+player.on('timeupdate', saveCurrentTime);
+
+function restorePlaybackTime() {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+
+    player.setCurrentTime(currentTime|| 0)
+}
+restorePlaybackTime();
